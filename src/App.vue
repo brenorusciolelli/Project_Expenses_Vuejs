136d34c8ad347676faae1572db0063330d0b2895
Breno Rusciolelli <breno.brito@acpgroup.com.br>
2021-08-04 11:36:37 -0300

    Create Component BaseSpinner

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <base-spinner/>
+    <!--<router-view/> -->
+  </div>
+</template>
+
+<script>
+import BaseSpinner from '@/components/global/BaseSpinner'
+
+export default {
+  name: 'App',
+  components: {
+    BaseSpinner
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>

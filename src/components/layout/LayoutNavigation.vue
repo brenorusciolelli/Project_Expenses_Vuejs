c7a295e76dfc5469120c7499178d04a7c8ba0114
Breno Rusciolelli <breno.brito@acpgroup.com.br>
2021-08-05 16:18:46 -0300

    Create Component New Expense

--- src/components/layout/LayoutNavigation.vue
+++ src/components/layout/LayoutNavigation.vue
@@ -1,5 +1,6 @@
 <template>
-  <nav class="nav flex-column">
+  <nav class="nav flex-column justify-content-between">
+    <div>
 <router-link
 exact
 :key="i"
@@ -11,10 +12,20 @@ v-for="(router, i) in routerLinks"
 <i class="fa" :class="`fa-${router.meta.icon}`"></i>
 {{ router.meta.title }}
 </router-link>
+<hr>
+<layout-new-expense/>
+</div>
+<layout-logout />
 </nav>
 </template>
 <script>
+import LayoutLogout from '@/components/layout/LayoutLogout'
+import LayoutNewExpense from '@/components/layout/LayoutNewExpense'
 export default {
+  components: {
+    LayoutLogout,
+    LayoutNewExpense
+  },
   computed: {
     routerLinks () {
       return this.$router.options.routes.filter(r => r.name !== 'login')

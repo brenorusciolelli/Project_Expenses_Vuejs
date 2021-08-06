2a9b687d2967642d95a6142d15832779b8c64229
Breno Rusciolelli <breno.brito@acpgroup.com.br>
2021-08-06 11:25:11 -0300

    Create button receipt

--- src/components/layout/LayoutNewExpense.vue
+++ src/components/layout/LayoutNewExpense.vue
@@ -10,8 +10,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLiveLabel">Adicionar um novo gasto</h5>
-        <button type="button" @click="closeModal()" class="close">
-          <span aria-hidden="true">×</span>
+        <button type="button" @click="closeModal()" class="btn-close btn-close-black" aria-label="Close">
         </button>
       </div>
       <div class="modal-body">
@@ -23,6 +22,32 @@
           <div class="form-group col-4">
             <label for="">Valor (R$)</label>
             <input type="text" class="form-control" required v-model="form.value">
+          </div>
+          <div class="form-group flex-column col-12 d-flex align-items-center">
+            <input
+            ref="input"
+            type="file"
+            class="d-none"
+            accept="image/*"
+            @change="handleFile($event)"
+            >
+            <button
+            type="button"
+            class="btn w-50 btn-outline-secondary mt-3"
+            @click="openFileDialog()"
+            >
+              Adicionar Comprovante
+            </button>
+            <div class="mt-2" v-if="form.receipt">
+              {{ form.receipt.name }}
+              <button
+              type="button"
+              @click="form.receipt = ''"
+              class="btn badge bagde-light"
+              >
+              <i class="fa fa-trash text-danger"></i>
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -42,32 +67,68 @@
 <script>
 export default {
   data: () => ({
+    loading: false,
     showModal: false,
     form: {
-      description: '',
-      value: ''
+      value: '',
+      receipt: '',
+      description: ''
     }
   }),
-  methods: {
-    submit () {
-      this.$root.$emit('Spinner::show')
-      const ref = this.$firebase.database().ref(window.uid)
-      const id = ref.push().key
-      const payload = {
-        id,
-        receipt: '',
-        value: this.form.value,
-        createdAt: new Date().getTime(),
-        description: this.form.description
+  computed: {
+    fileName () {
+      const { receipt } = this.form
+
+      if (receipt) {
+        const split = receipt.name.split('.')
+        return `${split[0]}-${new Date().getTime()}.${split[1]}`
+      } else {
+        return ''
       }
-      ref.child(id).set(payload, err => {
-        this.$root.$emit('Spinner::hide')
-        if (err) {
-          console.error(err)
-        } else {
-          this.closeModal()
+    }
+  },
+  methods: {
+    openFileDialog () {
+      this.$refs.input.value = null
+      this.$refs.input.click()
+    },
+    handleFile ({ target }) {
+      this.form.receipt = target.files[0]
+    },
+    async submit () {
+      let url = ''
+      this.loading = true
+
+      try {
+        const ref = this.$firebase.database().ref(window.uid)
+        const id = ref.push().key
+        if (this.form.receipt) {
+          const snapshot = await this.$firebase.storage()
+            .ref(window.uid)
+            .child(this.fileName)
+            .put(this.form.receipt)
+
+          url = await snapshot.ref.getDownloadURL()
         }
-      })
+        const payload = {
+          id,
+          ...this.form,
+          receipt: url,
+          createdAt: new Date().getTime()
+        }
+        ref.child(id).set(payload, err => {
+          this.$root.$emit('Spinner::hide')
+          if (err) {
+            console.error(err)
+          } else {
+            this.closeModal()
+          }
+        })
+      } catch (e) {
+        console.error(e)
+      } finally {
+        this.$root.$emit('Spinner::show')
+      }
     },
     closeModal () {
       this.showModal = false
@@ -80,4 +141,5 @@ export default {
 .modal {
   color: var(--darker);
 }
+
 </style>
